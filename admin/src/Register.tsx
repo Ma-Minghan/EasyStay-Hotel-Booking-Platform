35c76d1b1cb2770de29aa5117eaa995c29784846
MinghanMa <2024302111025@whu.edu.cn>
2026-02-09 19:53:30 +0800

    feat(admin): add register page and routing

--- admin/src/Register.tsx
+++ admin/src/Register.tsx
@@ -1,22 +1,22 @@
+import { Form, Input, Button, Card, Typography, Radio } from 'antd';
 import { Link } from 'react-router-dom';
-import { Form, Input, Button, Card, Typography } from 'antd';
 import './App.css';
 
 const { Title } = Typography;
 
-function App() {
+function Register() {
   const [form] = Form.useForm();
 
   const onFinish = (values: any) => {
-    console.log('Login values:', values);
-    // TODO: 之后这里接后端登录接口
+    console.log('Register values:', values);
+    // TODO: 之后这里接后端注册接口
   };
 
   return (
     <div className="login-page">
       <Card className="login-card">
         <Title level={3} style={{ textAlign: 'center', marginBottom: 24 }}>
-          易宿酒店后台管理登录
+          易宿酒店后台注册
         </Title>
         <Form
           form={form}
@@ -39,14 +39,26 @@ function App() {
             <Input.Password placeholder="请输入密码" />
           </Form.Item>
 
+          <Form.Item
+            label="角色"
+            name="role"
+            initialValue="merchant"
+            rules={[{ required: true, message: '请选择角色' }]}
+          >
+            <Radio.Group>
+              <Radio value="merchant">商户</Radio>
+              <Radio value="admin">管理员</Radio>
+            </Radio.Group>
+          </Form.Item>
+
           <Form.Item>
             <Button type="primary" htmlType="submit" block>
-              登录
+              注册
             </Button>
           </Form.Item>
 
           <div style={{ textAlign: 'right' }}>
-            还没有账号？<Link to="/register">去注册</Link>
+            已有账号？<Link to="/login">去登录</Link>
           </div>
         </Form>
       </Card>
@@ -54,4 +66,4 @@ function App() {
   );
 }
 
-export default App;
+export default Register;
--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import App from './App';
+import Register from './Register';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<App />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
